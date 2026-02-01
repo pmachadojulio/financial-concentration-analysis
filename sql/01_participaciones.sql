a68273cea6aaa5f158d112be3385efe98d35d25e
pmachadojulio <pmachado.julio@gmail.com>
2026-02-01 16:50:35 -0300

    Update 01_participaciones.sql

--- sql/01_participaciones.sql
+++ sql/01_participaciones.sql
@@ -1,1 +1,11 @@
-
+WITH participaciones AS (
+    SELECT 
+        cliente_id,
+        canal,
+        SUM(total_cliente) AS total_cliente,
+        SUM(SUM(total_cliente)) OVER (PARTITION BY canal) AS total_canal,
+        SUM(total_cliente) * 1.0 / SUM(SUM(total_cliente)) OVER (PARTITION BY canal) AS proporcion,
+        RANK() OVER (PARTITION BY canal ORDER BY SUM(total_cliente) DESC) AS rnk
+    FROM conciliacion_bancaria
+    GROUP BY cliente_id, canal
+),
