68ea9cc277c12e6deddaf918ee8380a045a69ca8
pmachadojulio <pmachado.julio@gmail.com>
2026-02-01 16:48:11 -0300

    Update diagnostico_canal.sql

--- sql/diagnostico_canal.sql
+++ sql/diagnostico_canal.sql
@@ -1,1 +1,8 @@
-
+diagnostico_canal AS (
+    SELECT
+        canal,
+        SUM(proporcion * proporcion) AS hhi,
+        -SUM(proporcion * LN(proporcion)) AS entropia
+    FROM participaciones
+    GROUP BY canal
+)
